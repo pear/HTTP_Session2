e4d39c78ddd289377274b436970259704ff15de6
Till Klampaeckel <till@php.net>
2007-11-23 07:45:54 +0000

    * cs fixes
    git-svn-id: http://svn.php.net/repository/pear/packages/HTTP_Session2/trunk@246814 c90b9560-bf6c-de11-be94-00142212c4b1

--- HTTP/Session2/Container.php
+++ HTTP/Session2/Container.php
@@ -1,47 +1,61 @@
 <?php
-//
-// +-----------------------------------------------------------------------+
-// | Copyright (c) 2004, Tony Bibbs                                        |
-// | All rights reserved.                                                  |
-// |                                                                       |
-// | Redistribution and use in source and binary forms, with or without    |
-// | modification, are permitted provided that the following conditions    |
-// | are met:                                                              |
-// |                                                                       |
-// | o Redistributions of source code must retain the above copyright      |
-// |   notice, this list of conditions and the following disclaimer.       |
-// | o Redistributions in binary form must reproduce the above copyright   |
-// |   notice, this list of conditions and the following disclaimer in the |
-// |   documentation and/or other materials provided with the distribution.|
-// | o The names of the authors may not be used to endorse or promote      |
-// |   products derived from this software without specific prior written  |
-// |   permission.                                                         |
-// |                                                                       |
-// | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   |
-// | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     |
-// | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR |
-// | A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  |
-// | OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, |
-// | SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT      |
-// | LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, |
-// | DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY |
-// | THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT   |
-// | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE |
-// | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  |
-// |                                                                       |
-// +-----------------------------------------------------------------------+
-// | Author: Alexander Radivanovich <info@wwwlab.net>                      |
-// }         Tony Bibbs <tony@geeklog.net>                                 |
-// +-----------------------------------------------------------------------+
-//
-
+/**
+ * +-----------------------------------------------------------------------+
+ * | Copyright (c) 2004, Tony Bibbs                                        |
+ * | All rights reserved.                                                  |
+ * |                                                                       |
+ * | Redistribution and use in source and binary forms, with or without    |
+ * | modification, are permitted provided that the following conditions    |
+ * | are met:                                                              |
+ * |                                                                       |
+ * | o Redistributions of source code must retain the above copyright      |
+ * |   notice, this list of conditions and the following disclaimer.       |
+ * | o Redistributions in binary form must reproduce the above copyright   |
+ * |   notice, this list of conditions and the following disclaimer in the |
+ * |   documentation and/or other materials provided with the distribution.|
+ * | o The names of the authors may not be used to endorse or promote      |
+ * |   products derived from this software without specific prior written  |
+ * |   permission.                                                         |
+ * |                                                                       |
+ * | THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS   |
+ * | "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT     |
+ * | LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR |
+ * | A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT  |
+ * | OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, |
+ * | SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT      |
+ * | LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, |
+ * | DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY |
+ * | THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT   |
+ * | (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE |
+ * | OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  |
+ * |                                                                       |
+ * +-----------------------------------------------------------------------+
+ * | Author: Alexander Radivanovich <info@wwwlab.net>                      |
+ * |         Tony Bibbs <tony@geeklog.net>                                 |
+ * +-----------------------------------------------------------------------+
+ *
+ * PHP version 5
+ *
+ * @category HTTP
+ * @package  HTTP_Session2
+ * @author   Alexander Radivaniovich <info@wwwlab.net>
+ * @author   Tony Bibbs <tony@geeklog.net>
+ * @license
+ * @version  CVS: $Id$
+ * @link     http://pear.php.net/package/HTTP_Session2
+ */
 require_once 'HTTP/Session2/ContainerInterface.php';
 
 /**
  * Container class for storing session data data
  *
- * @author  Alexander Radivaniovich <info@wwwlab.net>
- * @package HTTP_Session2
+ * @category HTTP
+ * @package  HTTP_Session2
+ * @author   Alexander Radivaniovich <info@wwwlab.net>
+ * @author   Tony Bibbs <tony@geeklog.net>
+ * @license
+ * @version  CVS: $Id$
+ * @link     http://pear.php.net/package/HTTP_Session2
  */
 abstract class HTTP_Session2_Container implements HTTP_Session2_Container_Interface
 {
@@ -56,8 +70,8 @@ abstract class HTTP_Session2_Container implements HTTP_Session2_Container_Interf
     /**
      * Constrtuctor method
      *
-     * @access public
-     * @param  array  $options Additional options for the container object
+     * @param array $options Additional options for the container object
+     *
      * @return void
      */
     public function __construct($options = null)
@@ -71,7 +85,7 @@ abstract class HTTP_Session2_Container implements HTTP_Session2_Container_Interf
     /**
      * Set some default options
      *
-     * @access private
+     * @return void
      */
     private function setDefaults()
     {
@@ -80,12 +94,13 @@ abstract class HTTP_Session2_Container implements HTTP_Session2_Container_Interf
     /**
      * Parse options passed to the container class
      *
-     * @access protected
-     * @param array Options
+     * @param array $options Options
+     *
+     * @return void
      */
     protected function parseOptions($options)
     {
-    	foreach ($options as $option => $value) {
+        foreach ($options as $option => $value) {
             if (in_array($option, array_keys($this->options))) {
                 $this->options[$option] = $value;
             }
@@ -100,15 +115,12 @@ abstract class HTTP_Session2_Container implements HTTP_Session2_Container_Interf
      */
     public function set()
     {
-        $GLOBALS['HTTP_Session2_Container'] =& $this;
         session_module_name('user');
-        session_set_save_handler(
-            array($this, 'open'),
+        session_set_save_handler(array($this, 'open'),
             array($this, 'close'),
             array($this, 'read'),
             array($this, 'write'),
             array($this, 'destroy'),
-            array($this, 'gc')
-        );
+            array($this, 'gc'));
     }
 }
